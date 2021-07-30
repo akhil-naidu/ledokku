0d2bd14c6e91fe3c8168a3740d52a4d6b35d1360
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2021-07-30 08:07:39 +0530

    Updating the downloader for multi-servings

--- ledokkucli.sh
+++ ledokkucli.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
+set -e
+
+function define-colors {
+
+  # Using tput will eliminate the usage of "-e" in echo, and can be used anywhere
+  ## Color Palet
+  ## Should exist in every script
+  RED="$(tput setaf 1)" # ${RED}
+  GREEN="$(tput setaf 2)" # ${GREEN}
+  YELLOW="$(tput setaf 3)" # ${YELLOW}
+  BLUE="$(tput setaf 123)" # ${BLUE}
+  END="$(tput setaf 7)" # ${END
+}
+
 
 function main(){
 
+    echo "${YELLOW}Downloading Script${END}"
     wget https://raw.githubusercontent.com/akhil-naidu/ledokku/new/ledokku-cli/ledokku
     wait
+    echo "${YELLOW}Installing Ledokku CLI${END}"
     sudo cat ledokku > /usr/local/bin/ledokku
     wait
+    echo "${BLUE}Making Executable${END}"
     sudo chmod +x /usr/local/bin/ledokku
     wait
-    # clear
+    echo "${YELLOW}Getting Ready For Ledokku Terminal Command${END}"
+    sudo exec $SHELL
     wait
-    echo "Close and reopen your terminal, for now this is the fastest way for ledokku to work with tab autofill"
-
+    # Cleaning up will ensure that the script always download the latest version(ledokkuCLI) without any conflicts in file name.
+    sudo rm -rf ledokku
 }
 
+define-colors
 if [ "$(whoami)" == "root" ] ; then
     main
 else
-    echo "Please! Run the script with root user"
+    echo "${RED}Warning! ${YELLOW}Run the script with ${BLUE}root${YELLOW} permissions ${END}"
     exit
 fi
